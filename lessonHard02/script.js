f59e38c5eb77a87a1108af962bd620e1f702a574
Egor Matveev <egormsm@gmail.com>
2020-11-20 15:00:07 +0300

    Усложненное задание

---
+++ lessonHard02/script.js
@@ -0,0 +1,17 @@
+
+let num = 266219;
+let str = String(num);
+let arr = str.split('');
+
+console.log(arr);
+
+let result = arr.reduce(function(a,b) {
+    "use strict";
+    return a*b;
+});
+ 
+console.log(result);
+
+let deg = result ** 3;
+let degstr = String(deg);
+console.log(degstr.substring(0,2));
