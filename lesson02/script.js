71febdb7e8508fde34f6bfda021e5651115208f9
Egor Matveev <egormsm@gmail.com>
2020-11-20 13:30:12 +0300

    Вывел необходимые значения в консоль

--- lesson02/script.js
+++ lesson02/script.js
@@ -1,9 +1,13 @@
 let money = 34973984;
+console.log (typeof money);
 let income = 'фриланс';
-let addExpenses = 'такси, еда, что-нибудь еще';
+console.log (typeof income);
+let addExpenses = 'Такси, Еда, Пивас';
 let deposit = 7 === '7';
+console.log (typeof deposit);
 let mission = 343434343434;
 let period = 7;
-
-alert ("Ас-саляму алейкум");
-console.log("тридцать четыре");
+console.log (addExpenses.length);
+console.log ("Период равен " + period + " месяцам");
+console.log ("Цель заработать " + mission + " гривен");
+console.log(addExpenses.toLowerCase(), addExpenses.split(', '));
