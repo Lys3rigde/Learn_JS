b973933dc2ed874d8b58d04d30d7d8a02ec54f49
Egor Matveev <egormsm@gmail.com>
2020-11-20 12:25:46 +0300

    Добавил значения к переменным

---
+++ lesson02/script.js
@@ -0,0 +1,9 @@
+let money = 34973984;
+let income = 'фриланс';
+let addExpenses = 'такси, еда, что-нибудь еще';
+let deposit = 7 === '7';
+let mission = 343434343434;
+let period = 7;
+
+alert ("Ас-саляму алейкум");
+console.log("тридцать четыре");
