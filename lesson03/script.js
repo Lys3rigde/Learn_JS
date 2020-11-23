3ade260135c0f3857fd168987b02941611625c65
Egor Matveev <egormsm@gmail.com>
2020-11-23 12:23:04 +0300

    lesson03

---
+++ lesson03/script.js
@@ -0,0 +1,15 @@
+let money = 34973984;
+console.log (typeof money);
+let income = 'фриланс';
+console.log (typeof income);
+let addExpenses = 'Такси, Еда, Пивас';
+let deposit = 7 === '7';
+console.log (typeof deposit);
+let mission = 343434343434;
+let period = 7;
+console.log (addExpenses.length);
+console.log ("Период равен " + period + " месяцам");
+console.log ("Цель заработать " + mission + " гривен");
+console.log(addExpenses.toLowerCase(), addExpenses.split(', '));
+let budgetDay = money/30;
+console.log(budgetDay);
